eb14641ab2cf96ffe0ad1ac3d9b126c20a19efdd
Martin deeagle Kock <mko@leibniz-psychology.org>
2024-04-03 13:40:26 +0200

    ci(local): add local executable ci

---
+++ run-ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set +x
+
+echo "Running local CI ..."
+
+echo "Running lints ..."
+
+echo "Lint markdown ..."
+docker run --rm -v "$(pwd):/app" docdee/mdlint -c /app/.markdownlint.yml -i CHANGELOG.md "**/*.md"
+
+echo "Lint shell scripts ..."
+docker run --rm -v "$(pwd):/app" koalaman/shellcheck-alpine:stable find /app -type f -name '*.sh' -exec shellcheck {} +
+
+echo "Lint code (golang) ..."
+docker run -t --rm -v ./src:/app -w /app golangci/golangci-lint:latest golangci-lint run --fast
+
+echo "CI successfully finished."
