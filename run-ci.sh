f2d2109c4c59ff49e2b8b04d5b2571818fef153d
Martin deeagle Kock <mko@leibniz-psychology.org>
2024-04-03 13:43:30 +0200

    ci(local): fix for run

--- run-ci.sh
+++ run-ci.sh
@@ -8,12 +8,12 @@ echo "Running local CI ..."
 echo "Running lints ..."
 
 echo "Lint markdown ..."
-docker run --rm -v "$(pwd):/app" docdee/mdlint -c /app/.markdownlint.yml -i CHANGELOG.md "**/*.md"
+docker run --rm -v "$(pwd):/app" docdee/mdlint -c /app/.markdownlint.yml -i CHANGELOG.md -i dist "**/*.md"
 
 echo "Lint shell scripts ..."
 docker run --rm -v "$(pwd):/app" koalaman/shellcheck-alpine:stable find /app -type f -name '*.sh' -exec shellcheck {} +
 
 echo "Lint code (golang) ..."
-docker run -t --rm -v ./src:/app -w /app golangci/golangci-lint:latest golangci-lint run --fast
+docker run -t --rm -v "$(pwd):/app" -w /app golangci/golangci-lint:latest golangci-lint run --fast
 
 echo "CI successfully finished."
